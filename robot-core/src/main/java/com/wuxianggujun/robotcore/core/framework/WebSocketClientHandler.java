aa9d9ca2e721ad99590f70a4e4d90bdc42a912a8
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-30 22:26:46 +0800

    添加WebSocket配置类监听服务器的消息

---
+++ robot-core/src/main/java/com/wuxianggujun/robotcore/core/framework/WebSocketClientHandler.java
@@ -0,0 +1,81 @@
+package com.wuxianggujun.robotcore.core.framework;
+
+import io.netty.channel.*;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+public class WebSocketClientHandler extends SimpleChannelInboundHandler<Object> {
+    private final WebSocketClientHandshaker handshakes;
+    private ChannelPromise handshakeFuture;
+    //序列化JSON转化为对象
+    //private ObjectMapper objectMapper;
+
+    public WebSocketClientHandler(WebSocketClientHandshaker handshakes) {
+        this.handshakes = handshakes;
+        //this.objectMapper = new ObjectMapper();
+    }
+
+    public ChannelFuture handshakeFuture() {
+        return handshakeFuture;
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) {
+        handshakeFuture = ctx.newPromise();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        handshakes.handshake(ctx.channel());
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("WebSocket Client disconnected!");
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Channel ch = ctx.channel();
+        if (!handshakes.isHandshakeComplete()) {
+            try {
+                handshakes.finishHandshake(ch, (FullHttpResponse) msg);
+                System.out.println("WebSocket 客户端已连接！");
+                handshakeFuture.setSuccess();
+            } catch (WebSocketHandshakeException e) {
+                System.out.println("WebSocket Client failed to connect");
+                handshakeFuture.setFailure(e);
+            }
+            return;
+        }
+
+        if (msg instanceof FullHttpResponse) {
+            FullHttpResponse response = (FullHttpResponse) msg;
+            throw new IllegalStateException(
+                    "Unexpected FullHttpResponse (getStatus=" + response.status() +
+                            ", content=" + response.content().toString(CharsetUtil.UTF_8) + ')');
+        }
+
+        WebSocketFrame frame = (WebSocketFrame) msg;
+        if (frame instanceof TextWebSocketFrame) {
+            TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
+            //objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            System.out.println("WebSocket 客户端收到消息: " + textFrame.text());
+        } else if (frame instanceof PongWebSocketFrame) {
+            System.out.println("WebSocket Client received pong");
+        } else if (frame instanceof CloseWebSocketFrame) {
+            System.out.println("WebSocket Client received closing");
+            ch.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        if (!handshakeFuture.isDone()) {
+            handshakeFuture.setFailure(cause);
+        }
+        ctx.close();
+    }
+}
--- robot-web/src/main/java/com/wuxianggujun/RobotWebApplication.java
+++ robot-web/src/main/java/com/wuxianggujun/RobotWebApplication.java
@@ -1,13 +1,20 @@
 package com.wuxianggujun;
 
+import com.wuxianggujun.robotcore.core.framework.WebSocketClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RobotWebApplication {
+@SpringBootApplication(scanBasePackages = "com.wuxianggujun")
+public class RobotWebApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RobotWebApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        WebSocketClient.main(args);
+    }
 }
