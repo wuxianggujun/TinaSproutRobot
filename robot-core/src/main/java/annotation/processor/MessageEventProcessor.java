e0b591cb6357d6280dd97b5b3ef28d8ff3aedfdb
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-07-06 09:57:23 +0800

    初始化几个类

--- robot-core/src/main/java/annotation/processor/MessageEventProcessor.java
+++ robot-core/src/main/java/annotation/processor/MessageEventProcessor.java
@@ -1,13 +1,16 @@
 package annotation.processor;
 
-import com.google.auto.service.AutoService;
 import annotation.MessageEvent;
 import annotation.utils.Logger;
+import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,21 +26,40 @@ public class MessageEventProcessor extends AbstractProcessor {
     private Filer filer;
     private Logger logger;
 
+    private Types typeUtils;
+
+    private Elements elementUtils;
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         logger = new Logger(processingEnv.getMessager());
         filer = processingEnv.getFiler();
+        typeUtils = processingEnv.getTypeUtils();
+        elementUtils = processingEnv.getElementUtils();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(MessageEvent.class);
-        logger.i("-----------Hello world!------------");
+        for (TypeElement typeElement : annotations) {
+            for (Element e : roundEnv.getElementsAnnotatedWith(typeElement)) {
 
-        for (Element element : elements) {
-            TypeElement typeElement = (TypeElement) element;
-            logger.i(typeElement.getSimpleName().toString());
+                //获取注解
+                MessageEvent messageEvent = e.getAnnotation(MessageEvent.class);
+
+                //元素名称并将首字母大写
+                String name = e.getSimpleName().toString();
+                //包裹注解元素的元素, 也就是其父元素, 比如注解了成员变量或者成员函数, 其上层就是该类
+                Element enclosingElement = e.getEnclosingElement();
+                //获取父元素的全类名,用来生成报名
+                String enclosingQualifiedname;
+                if (enclosingElement instanceof PackageElement) {
+                    enclosingQualifiedname = ((PackageElement) enclosingElement).getQualifiedName().toString();
+                } else {
+                    enclosingQualifiedname = ((TypeElement) enclosingElement).getQualifiedName().toString();
+                }
+                logger.i(enclosingQualifiedname);
+            }
         }
         return false;
     }
--- robot-web/src/main/java/com/wuxianggujun/robotweb/test/Test.java
+++ robot-web/src/main/java/com/wuxianggujun/robotweb/test/Test.java
@@ -1,8 +1,11 @@
 package com.wuxianggujun.robotweb.test;
 
+import annotation.MessageEvent;
+import annotation.enums.MessageEventType;
 import com.wuxianggujun.robotcore.annotation.BotAnnotation;
 import com.wuxianggujun.robotcore.core.bot.Bot;
 
+@MessageEvent(MessageEventType.NOTICE)
 public class Test {
     @BotAnnotation
     private int i = 1000;
